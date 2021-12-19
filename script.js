6c964725ee49fefbf041dd03ae468357edcdbb7f
Raique Ramos <raiqueramos1@gmail.com>
2021-12-19 11:16:45 -0300

    Att0.1

---
+++ script.js
@@ -0,0 +1,732 @@
+ScrollReveal().reveal('.Badge', { 
+    duration: 50,
+    delay: 900,
+    origin: 'left',
+    distance: '300px'
+ });
+ScrollReveal().reveal('.Txt-2', { 
+    duration: 100,
+    delay: 900,
+    origin: 'left',
+    distance: '300px'
+ });
+ScrollReveal().reveal('.Paragraph-1', { 
+    duration: 200,
+    delay: 900,
+    origin: 'left',
+    distance: '300px'
+ });
+ScrollReveal().reveal('.Paragraph-2', { 
+    duration: 200,
+    delay: 1000,
+    origin: 'left',
+    distance: '300px'
+ });
+ScrollReveal().reveal('.EmailBtn', { 
+    duration: 300,
+    delay: 900,
+    origin: 'left',
+    distance: '300px'
+ });
+ScrollReveal().reveal('.Btn-3', { 
+    duration: 300,
+    delay: 900,
+    origin: 'left',
+    distance: '300px'
+ });
+ScrollReveal().reveal('.ImgPhone-1', { 
+    duration: 400,
+    delay: 900,
+    origin: 'bottom',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.fa-bars', { 
+    duration: 600,
+    delay: 100,
+    origin: 'top',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.LogoDlex', { 
+    duration: 400,
+    delay: 100,
+    origin: 'top',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Txt-3', { 
+    duration: 400,
+    delay: 100,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Paragraph-3', { 
+    duration: 700,
+    delay: 200,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FeaturesRectangle', { 
+    duration: 750,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Paragraph-4', { 
+    duration: 800,
+    delay: 300,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Paragraph-5', { 
+    duration: 900,
+    delay: 400,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Paragraph-6', { 
+    duration: 1000,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FeaturesImg', { 
+    duration: 1200,
+    delay: 500,
+    origin: 'right',
+    distance: '10px',
+ });
+
+
+ScrollReveal().reveal('.Txt-4', { 
+    duration: 1000,
+    delay: 1000,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Txt-5', { 
+    duration: 1200,
+    delay: 1200,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Btn-4', { 
+    duration: 1300,
+    delay: 1300,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Btn-5', { 
+    duration: 1400,
+    delay: 1400,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+
+
+
+ScrollReveal().reveal('.Pricing_Title', { 
+    duration: 500,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Pricing_Paragraph', { 
+    duration: 600,
+    delay: 600,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.PricingLife_TimeBox', { 
+    duration: 700,
+    delay: 700,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.LifeTime_Title', { 
+    duration: 800,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.PricingLife_TimeBox', { 
+    duration: 700,
+    delay: 700,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+/* FAQ */
+
+
+ScrollReveal().reveal('.FAQ-BOX-2', { 
+    duration: 500,
+    delay: 500,
+    origin: 'left',
+    distance: '20px',
+ });
+ScrollReveal().reveal('.FAQ-BOX-3', { 
+    duration: 500,
+    delay: 500,
+    origin: 'right',
+    distance: '20px',
+ });
+ScrollReveal().reveal('.FAQ-BOX-5', { 
+    duration: 700,
+    delay: 700,
+    origin: 'left',
+    distance: '20px',
+ });
+ScrollReveal().reveal('.FAQ-BOX-6', { 
+    duration: 700,
+    delay: 700,
+    origin: 'right',
+    distance: '20px',
+ });
+
+
+/* NEWSLETTER */
+
+
+ScrollReveal().reveal('.Newsletter-Container-Title', { 
+    duration: 500,
+    delay: 500,
+    origin: 'left',
+    distance: '20px',
+ });
+ScrollReveal().reveal('.Newsletter-Container-Paragraph', { 
+    duration: 600,
+    delay: 600,
+    origin: 'left',
+    distance: '20px',
+ });
+ScrollReveal().reveal('.InputBtn', { 
+    duration: 700,
+    delay: 700,
+    origin: 'right',
+    distance: '20px',
+ });
+ScrollReveal().reveal('.Newsletter-InputBtn-Paragraph', { 
+    duration: 800,
+    delay: 800,
+    origin: 'right',
+    distance: '20px',
+ });
+
+ /* B L O G */
+
+
+ScrollReveal().reveal('.SubTitle-Container', { 
+    duration: 500,
+    delay: 500,
+    origin: 'left',
+    distance: '100px',
+ });
+ScrollReveal().reveal('.Title-Container', { 
+    duration: 700,
+    delay: 500,
+    origin: 'left',
+    distance: '100px',
+ });
+ScrollReveal().reveal('.Paragraph-Container', { 
+    duration: 800,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+ScrollReveal().reveal('.CaseStudy', { 
+    duration: 800,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Video', { 
+    duration: 1000,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Article', { 
+    duration: 1200,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+
+
+ScrollReveal().reveal('.Contact-Title', { 
+    duration: 300,
+    delay: 300,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Contact-Paragraph', { 
+    duration: 400,
+    delay: 400,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Contact-Title2', { 
+    duration: 500,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Contact-Title3', { 
+    duration: 600,
+    delay: 600,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+
+
+
+ScrollReveal().reveal('.Contact-Container-Title', { 
+    duration: 500,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormYN', { 
+    duration: 650,
+    delay: 650,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormPN', { 
+    duration: 750,
+    delay: 750,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormEA', { 
+    duration: 850,
+    delay: 850,
+    origin: 'right',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormS', { 
+    duration: 950,
+    delay: 950,
+    origin: 'right',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormM', { 
+    duration: 1050,
+    delay: 1050,
+    origin: 'right',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#fname', { 
+    duration: 700,
+    delay: 700,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#ftel', { 
+    duration: 800,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#lemail', { 
+    duration: 900,
+    delay: 900,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#ltext', { 
+    duration: 1000,
+    delay: 1000,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#Message', { 
+    duration: 1100,
+    delay: 1100,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormBtn', { 
+    duration: 1200,
+    delay: 1200,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+ScrollReveal().reveal('.FormYN', { 
+    duration: 650,
+    delay: 650,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormPN', { 
+    duration: 750,
+    delay: 750,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormEA', { 
+    duration: 850,
+    delay: 850,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormS', { 
+    duration: 950,
+    delay: 950,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormM', { 
+    duration: 1050,
+    delay: 1050,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#fname', { 
+    duration: 700,
+    delay: 700,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#ftel', { 
+    duration: 800,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#lemail', { 
+    duration: 900,
+    delay: 900,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#ltext', { 
+    duration: 1000,
+    delay: 1000,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('#Message', { 
+    duration: 1100,
+    delay: 1100,
+    origin: 'right',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.FormBtn', { 
+    duration: 1200,
+    delay: 1200,
+    origin: 'right',
+    distance: '1000px',
+ });
+
+
+
+
+
+
+ScrollReveal().reveal('.Title3-Team', { 
+    duration: 800,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Title1-Team', { 
+    duration: 900,
+    delay: 900,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Container-PersonOne', { 
+    duration: 1000,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Container-PersonTwo', { 
+    duration: 1500,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Container-PersonThree', { 
+    duration: 1900,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+
+
+
+
+ScrollReveal().reveal('.StatsTitle', { 
+    duration: 800,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Stats100', { 
+    duration: 900,
+    delay: 900,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Stats24_7', { 
+    duration: 1000,
+    delay: 1000,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Stats250K', { 
+    duration: 1100,
+    delay: 1100,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Stats-Container', { 
+    duration: 1900,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+
+
+
+
+ScrollReveal().reveal('.LogoClouds_Txt', { 
+    duration: 800,
+    delay: 800,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.LogoClouds_Title', { 
+    duration: 900,
+    delay: 900,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.LogoClouds_Paragraph', { 
+    duration: 1000,
+    delay: 1000,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.LogoClouds_Btn', { 
+    duration: 1100,
+    delay: 1100,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Btn_One', { 
+    duration: 1200,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Btn_Two', { 
+    duration: 1600,
+    delay: 200,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+ScrollReveal().reveal('.LogoClouds_Logo', { 
+    duration: 1000,
+    delay: 500,
+    origin: 'right',
+    distance: '30px',
+ });
+ScrollReveal().reveal('.Box_LogoClouds_Logo', { 
+    duration: 1100,
+    delay: 500,
+    origin: 'right',
+    distance: '300px',
+ });
+ScrollReveal().reveal('.Box_ImgDocker', { 
+    duration: 1200,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Box_ImgVerce', { 
+    duration: 1250,
+    delay: 500,
+    origin: 'right',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Box_ImgMedium', { 
+    duration: 1300,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Box_ImgYoutube', { 
+    duration: 1350,
+    delay: 500,
+    origin: 'right',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Box_ImgGoogle', { 
+    duration: 1400,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Box_ImgNetFlix', { 
+    duration: 1450,
+    delay: 500,
+    origin: 'right',
+    distance: '1000px',
+ });
+
+
+ScrollReveal().reveal('.Footer_Paragraph', { 
+    duration: 1400,
+    delay: 500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Icons', { 
+    duration: 1550,
+    delay: 600,
+    origin: 'right',
+    distance: '1000px',
+ });
+
+
+/* P R I C I N G   H E A D E R */
+
+
+ScrollReveal().reveal('.BoxTitle_ParagraphI', { 
+    duration: 1000,
+    delay: 1000,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.BoxTitle_H1', { 
+    duration: 1200,
+    delay: 1200,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+/* B A R   P R I C I N G */
+ 
+
+ScrollReveal().reveal('.BarI', { 
+    duration: 1500,
+    delay: 1500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.BarII', { 
+    duration: 1400,
+    delay: 1400,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.BarIII', { 
+    duration: 1300,
+    delay: 1300,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+
+ /* P R I C I N G   M A I N */
+
+
+ScrollReveal().reveal('.BoxStarter_H5', { 
+    duration: 1500,
+    delay: 1500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.ThreeOptions_BoxH6', { 
+    duration: 1400,
+    delay: 1400,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.BoxStarter_H1', { 
+    duration: 1300,
+    delay: 1300,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Btn_Starter', { 
+    duration: 1200,
+    delay: 1200,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+ 
+
+ScrollReveal().reveal('.BoxDlexPlus_H5', { 
+    duration: 1550,
+    delay: 1550,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.ThreeOptions_BoxH6', { 
+    duration: 1450,
+    delay: 1450,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.BoxDlexPlus_H1', { 
+    duration: 1350,
+    delay: 1350,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Btn_DlexPlus', { 
+    duration: 1250,
+    delay: 1250,
+    origin: 'left',
+    distance: '1000px',
+ });
+
+ 
+
+ScrollReveal().reveal('.BoxDlexPro_H5', { 
+    duration: 1600,
+    delay: 1600,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.ThreeOptions_BoxH6', { 
+    duration: 1500,
+    delay: 1500,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.BoxDlexPro_H1', { 
+    duration: 1400,
+    delay: 1400,
+    origin: 'left',
+    distance: '1000px',
+ });
+ScrollReveal().reveal('.Btn_DlexPro', { 
+    duration: 1300,
+    delay: 1300,
+    origin: 'left',
+    distance: '1000px',
+ });
+
